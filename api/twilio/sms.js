cfb48ab36e7be4023803eadaff8ad3010c229502
calmcalluk-hub <calmcalluk@gmail.com>
2026-09-02 23:54:23 +0100

    Replace SMS webhook with callback confirmation handler

--- api/twilio/sms.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  const twiml = `<?xml version="1.0" encoding="UTF-8"?>
-<Response></Response>`;
-  res.setHeader('Content-Type', 'text/xml');
-  res.status(200).send(twiml);
-}
